10a8adcf319c6a5ccd281a96c13770b3daec7c95
Jacob Duijzer <jacob@duijzer.com>
2023-06-28 16:10:28 +0200

    US2-red: number of items as input parameter

---
+++ src/ElephantCarpaccio.UnitTests/RetailCalculatorShould.cs
@@ -0,0 +1,14 @@
+namespace ElephantCarpaccio.UnitTests;
+
+public class RetailCalculatorShould
+{
+   [Fact]
+   public void AcceptNumberOfItemsAsParameter()
+   {
+      RetailCalculator retailCalculator = new();
+
+      retailCalculator.SetNumberOfItems(10);
+      
+      Assert.Equal(10, retailCalculator.NumberOfItems);
+   }
+}
