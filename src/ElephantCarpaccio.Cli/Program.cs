0c8079c117fe940c7795ead0fe64b2b204263bbc
Jacob Duijzer <jacob@duijzer.com>
2023-06-29 10:50:46 +0200

    US11: menu with options

--- src/ElephantCarpaccio.Cli/Program.cs
+++ src/ElephantCarpaccio.Cli/Program.cs
@@ -1,24 +1,54 @@
 ﻿using ElephantCarpaccio.Cli;
 
-Console.WriteLine("Hello, Customer!");
-Console.Write("Number of items: ");
-var numberOfItemsInput = Console.ReadLine();
-if (!int.TryParse(numberOfItemsInput, out int numberOfItems))
-    throw new ArgumentException($"Can not parse {numberOfItemsInput} to a number");
+bool showMenu = true;
+while (showMenu)
+{
+    showMenu = MainMenu();
+}
 
-Console.Write("Price per item: ");
-var pricePerItemString = Console.ReadLine();
-if (!decimal.TryParse(pricePerItemString, out decimal pricePerItem))
-    throw new ArgumentException($"Can not parse {pricePerItemString} to a number");
+static bool MainMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, Customer!");
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1) Calculate retail price");
+    Console.WriteLine("2) Exit");
+    Console.Write("\r\nSelect an option: ");
+ 
+    switch (Console.ReadLine())
+    {
+        case "1":
+            CalculateRetailPrice();
+            return true;
+        
+        default:
+            return false;
+    }
+}
 
-Console.Write("State (for tax): ");
-var stateForTax = Console.ReadLine();
-var taxPercentage = new TaxService().GetPercentageForState(stateForTax);
+static void CalculateRetailPrice()
+{
+    Console.Write("Number of items: ");
+    var numberOfItemsInput = Console.ReadLine();
+    if (!int.TryParse(numberOfItemsInput, out int numberOfItems))
+        throw new ArgumentException($"Can not parse {numberOfItemsInput} to a number");
 
-Console.WriteLine($"Tax percentage for state '{stateForTax}': {taxPercentage}%");
-RetailCalculator retailCalculator = new(new DiscountService());
-retailCalculator.SetNumberOfItems(numberOfItems);
-retailCalculator.SetPricePerItem(pricePerItem);
-retailCalculator.SetTaxPercentage(taxPercentage);
+    Console.Write("Price per item: ");
+    var pricePerItemString = Console.ReadLine();
+    if (!decimal.TryParse(pricePerItemString, out decimal pricePerItem))
+        throw new ArgumentException($"Can not parse {pricePerItemString} to a number");
 
-Console.WriteLine($"The total price is: {retailCalculator.CalculateTotal()}");
+    Console.Write("State (for tax): ");
+    var stateForTax = Console.ReadLine();
+    var taxPercentage = new TaxService().GetPercentageForState(stateForTax);
+
+    Console.WriteLine($"Tax percentage for state '{stateForTax}': {taxPercentage}%");
+    RetailCalculator retailCalculator = new(new DiscountService());
+    retailCalculator.SetNumberOfItems(numberOfItems);
+    retailCalculator.SetPricePerItem(pricePerItem);
+    retailCalculator.SetTaxPercentage(taxPercentage);
+
+    Console.WriteLine($"The total price is: {retailCalculator.CalculateTotal()}");
+    Console.WriteLine("Press enter to continue");
+    Console.ReadLine();
+}
