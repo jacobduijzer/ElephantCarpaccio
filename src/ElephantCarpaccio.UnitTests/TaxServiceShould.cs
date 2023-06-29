2c53a22b64ca368cf14e7d6599519ca649caea42
Jacob Duijzer <jacob@duijzer.com>
2023-06-29 08:58:49 +0200

    US9-refactor: 2-Letter state code as input to select tax rate, add test scenarios

--- src/ElephantCarpaccio.UnitTests/TaxServiceShould.cs
+++ src/ElephantCarpaccio.UnitTests/TaxServiceShould.cs
@@ -4,13 +4,16 @@ namespace ElephantCarpaccio.UnitTests;
 
 public class TaxServiceShould
 {
-    [Fact]
-    public void SelectTaxCorrespondingToState()
+    [Theory]
+    [InlineData("UT", 6.85)]
+    [InlineData("AL", 4)]
+    [InlineData("??", 0)]
+    public void SelectTaxCorrespondingToState(string state, decimal expectedTaxPercentage)
     {
         TaxService taxService = new();
 
-        var taxPercentage = taxService.GetPercentageForState("TX");
+        var taxPercentage = taxService.GetPercentageForState(state);
 
-        Assert.Equal(6.25m, taxPercentage); 
+        Assert.Equal(expectedTaxPercentage, taxPercentage); 
     }
 }
