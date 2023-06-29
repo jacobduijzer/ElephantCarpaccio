9e7a13f15a5c56f2bca78ba9e0374082d57e8a17
Jacob Duijzer <jacob@duijzer.com>
2023-06-29 09:07:12 +0200

    US9-refactor: 2-Letter state code as input to select tax rate, console app using state and tax

--- src/ElephantCarpaccio.Cli/Program.cs
+++ src/ElephantCarpaccio.Cli/Program.cs
@@ -11,7 +11,14 @@ var pricePerItemString = Console.ReadLine();
 if (!decimal.TryParse(pricePerItemString, out decimal pricePerItem))
     throw new ArgumentException($"Can not parse {pricePerItemString} to a number");
 
+Console.Write("State (for tax): ");
+var stateForTax = Console.ReadLine();
+var taxPercentage = new TaxService().GetPercentageForState(stateForTax);
+
+Console.WriteLine($"Tax percentage for state '{stateForTax}': {taxPercentage}%");
 RetailCalculator retailCalculator = new();
 retailCalculator.SetNumberOfItems(numberOfItems);
 retailCalculator.SetPricePerItem(pricePerItem);
+retailCalculator.SetTaxPercentage(taxPercentage);
+
 Console.WriteLine($"The total price is: {retailCalculator.CalculateTotal()}");
